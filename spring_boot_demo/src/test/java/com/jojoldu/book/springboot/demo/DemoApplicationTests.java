44c2ef866eb524e7f1bec983f3c95fb7f4d76e11
kibeom <kibeom2000@naver.com>
2024-06-23 07:47:52 +0900

    add files spring_boot_setting

---
+++ spring_boot_demo/src/test/java/com/jojoldu/book/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot.demo;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
