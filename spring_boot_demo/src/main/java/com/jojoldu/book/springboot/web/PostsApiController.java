36271475ae87d9c77afc8b461086b0cc60660d05
kibeom <kibeom2000@naver.com>
2024-06-28 07:24:23 +0900

    add JPA files

--- spring_boot_demo/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ spring_boot_demo/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -2,10 +2,9 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,7 +13,11 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-
         return postsService.save(requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+
     }
 }
